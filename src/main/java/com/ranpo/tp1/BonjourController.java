527d10b349d3e2fb580417dec30fcd0be6a0010c
BRH5509A <hugo.berthomieu@etu.iut-tlse3.fr>
2026-09-14 11:58:24 +0200

    Initialisation du projet Spring Boot

---
+++ src/main/java/com/ranpo/tp1/BonjourController.java
@@ -0,0 +1,13 @@
+package com.ranpo.tp1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BonjourController {
+
+    @GetMapping("/bonjour")
+    public String bonjour() {
+        return "Bonjour le monde !";
+    }
+}
